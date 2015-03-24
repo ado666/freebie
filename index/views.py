e7fa55bbe98ed7b64cd37040511ca87111eaef15
ado <31337>
2015-03-24 16:29:22 +0300

    gitignore

--- index/views.py
+++ index/views.py
@@ -3,10 +3,11 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+import urllib2
 
 # Create your views here.
 def main(request):
-    post_data = [{address: 'Ð³Ð¾Ð´+ÐÐ¾ÑÐºÐ²Ð°,+ÐÐ¾ÑÐºÐ²Ð°,+Ð£ÑÐ°Ð»ÑÑÐºÐ°Ñ+ÑÐ»Ð¸ÑÐ°'}]     # a sequence of two element tuples
+    post_data = [{address: 'город Москва, Москва, Уральская улица'}]     # a sequence of two element tuples
     result = urllib2.urlopen('http://example.com', urllib.urlencode(post_data))
     content = result.read()
     
