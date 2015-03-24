75ff09f7ae7d4dab4dcede52b82fba20614ad202
ado <31337>
2015-03-24 16:32:51 +0300

    fix

--- index/views.py
+++ index/views.py
@@ -7,8 +7,8 @@ import urllib2
 
 # Create your views here.
 def main(request):
-    post_data = [{address: 'город Москва, Москва, Уральская улица'}]     # a sequence of two element tuples
-    result = urllib2.urlopen('http://example.com', urllib.urlencode(post_data))
+    post_data = [{'address': 'город Москва, Москва, Уральская улица'}]     # a sequence of two element tuples
+    result = urllib2.urlopen('http://example.com', urllib2.urlencode(post_data))
     content = result.read()
     
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index."+content)
