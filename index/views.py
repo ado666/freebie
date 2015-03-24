dba399426b770009b0fdda365d5bacfef49563ee
ado <31337>
2015-03-24 16:43:05 +0300

    debug

--- index/views.py
+++ index/views.py
@@ -10,5 +10,7 @@ def main(request):
     post_data = {'address': 'город+Москва,+Москва,+Уральская+улица'}     # a sequence of two element tuples
     result = urllib2.urlopen('https://maps.googleapis.com/maps/api/geocode/json', urllib.urlencode(post_data))
     content = result.read()
+
+    print urllib.urlencode(post_data)
     
     return HttpResponse("Hello, world. You're at the polls index."+content)
