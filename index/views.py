3dc60fa84ef9aea1bc4af64badbe3f5651280855
ado <31337>
2015-03-24 16:34:03 +0300

    fix2

--- index/views.py
+++ index/views.py
@@ -3,12 +3,12 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-import urllib2
+import urllib2, urllib
 
 # Create your views here.
 def main(request):
     post_data = [{'address': 'город Москва, Москва, Уральская улица'}]     # a sequence of two element tuples
-    result = urllib2.urlopen('http://example.com', urllib2.urlencode(post_data))
+    result = urllib2.urlopen('http://example.com', urllib.urlencode(post_data))
     content = result.read()
     
     return HttpResponse("Hello, world. You're at the polls index."+content)
