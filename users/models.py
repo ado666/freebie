4fcbfc9fa21355f78bf251d1229ad221d723ae53
ado <31337>
2015-04-02 02:15:01 +0300

    inittial user commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    companies_created = models.IntegerField(default=0)
+    offers_created = models.IntegerField(default=0)
+
+    notifications_sended = models.IntegerField(default=0)
+    notifications_readed = models.IntegerField(default=0)
