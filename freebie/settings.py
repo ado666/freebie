84a3b4c31da8cb39d235708aefd346502381e394
ado <31337>
2015-04-18 16:57:08 +0300

    new nginx config

--- freebie/settings.py
+++ freebie/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'freebie',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
+        'USER': 'freebie',                      # Not used with sqlite3.
         'PASSWORD': '31337',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -106,6 +106,7 @@ ROOT_URLCONF = 'freebie.urls'
 WSGI_APPLICATION = 'freebie.wsgi.application'
 
 TEMPLATE_DIRS = (
+    "index/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- index/views.py
+++ index/views.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from django.shortcuts import render_to_response
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 from django.shortcuts import render
 import urllib2, urllib
 
 # Create your views here.
-def main(request):
-    post_data = {'address': 'город Москва, Москва, Уральская улица'.encode(encoding='utf-8')}     # a sequence of two element tuples
-    result = urllib2.urlopen('https://maps.googleapis.com/maps/api/geocode/json', urllib.urlencode(post_data))
-    content = result.read()
-
-    print urllib.urlencode(post_data)
+def index(request):
+    # post_data = {'address': 'asd'}     # a sequence of two element tuples
+    # result = urllib2.urlopen('https://maps.googleapis.com/maps/api/geocode/json', urllib.urlencode(post_data))
+    # content = result.read()
+    # t = get_template('main.html')
+    # t = template.Template('My name is {{ name }}.')
+    # c = template.Context({'name': 'Adrian'})
+    # return t.render()
+    return render_to_response('main.html', {'users': ['msy', 'asd']})
     
-    return HttpResponse("Hello, world. You're at the polls index."+content)
+    # return HttpResponse("Hello, world. You're at the polls index."+content)
