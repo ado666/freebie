874195b4a4b2846f5f390d34331519e77b34a10b
Boris Kuznecov <b.kuznecov@gmail.com>
2016-04-11 15:48:21 +0400

    ignore local

---
+++ settings_local.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'freebie',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '31337',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+                    'charset': 'utf8',
+                    'use_unicode': True, },
+    }
+}
